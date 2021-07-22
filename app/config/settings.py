c7264cc3c1126cff826c9a14a6da21274c369794
Jędrzej Jamnicki <jedrzej.jamnicki@gmail.com>
2021-07-22 10:45:30 +0200

    Set up keycloak configuration

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,9 +1,12 @@
+# flake8: noqa
+
 import os
+import json
 
 from datetime import timedelta
 
 # FLASK
-DEBUG = False
+DEBUG = False  # celery workers may not work correctly if DEBUG=True, possible conflict with flask reloader
 DEBUG_TB_ENABLED = False
 DEBUG_TB_HOSTS = 'localhost'
 DEBUG_TB_INTERCEPT_REDIRECTS = False
@@ -12,18 +15,45 @@ DEFAULT_CHARSET = 'utf-8'
 LOG_LEVEL = 'DEBUG'
 
 SERVER_NAME = os.environ.get('SERVER_NAME')
+SERVER_ADDRESS = SERVER_NAME.split(':')[0]
+SERVER_PORT = SERVER_NAME.split(':')[-1]
+WWW_PROTOCOL = "http"
 
 REMEMBER_COOKIE_DURATION = timedelta(minutes=30)
 SECRET_KEY = '!$flhgsdf324NO%$#SOET!$!'
 
-# OpenIDConnect
-OIDC_CLIENT_SECRETS = 'config/client_secrets.json'
-OIDC_ID_TOKEN_COOKIE_SECURE = False
-OIDC_REQUIRE_VERIFIED_EMAIL = False
+# Keycloak
+KEYCLOAK_ADDRESS = os.environ.get('KEYCLOAK_ADDRESS')
+KEYCLOAK_SERVER_PORT = os.environ.get('KEYCLOAK_SERVER_PORT')
+KEYCLOAK_REALM_NAME = os.environ.get('KEYCLOAK_REALM_NAME')
+KEYCLOAK_CLIENT_ID = os.environ.get('KEYCLOAK_CLIENT_ID')
+KEYCLOAK_ADMIN_ROLE = os.environ.get('KEYCLOAK_ADMIN_ROLE')
+
+# Flask-OIDC
+client_secrets = {
+    "web": {
+        "auth_uri": f"{WWW_PROTOCOL}://{SERVER_ADDRESS}:{KEYCLOAK_SERVER_PORT}/auth/realms/{KEYCLOAK_REALM_NAME}/protocol/openid-connect/auth",
+        "client_id": KEYCLOAK_CLIENT_ID,
+        "client_secret": SECRET_KEY,
+        "redirect_uris": [
+            f"{WWW_PROTOCOL}://{SERVER_ADDRESS}:{SERVER_PORT}/oidc_callback"
+        ],
+        "userinfo_uri": f"{WWW_PROTOCOL}://{KEYCLOAK_ADDRESS}:{KEYCLOAK_SERVER_PORT}/auth/realms/{KEYCLOAK_REALM_NAME}/protocol/openid-connect/userinfo",
+        "token_uri": f"{WWW_PROTOCOL}://{KEYCLOAK_ADDRESS}:{KEYCLOAK_SERVER_PORT}/auth/realms/{KEYCLOAK_REALM_NAME}/protocol/openid-connect/token",
+        "token_introspection_uri": f"{WWW_PROTOCOL}://{KEYCLOAK_ADDRESS}:{KEYCLOAK_SERVER_PORT}/auth/realms/{KEYCLOAK_REALM_NAME}/protocol/openid-connect/token/introspect"
+    }
+}
+
+OIDC_CLIENT_SECRETS = "config/client_secrets.json"
+OIDC_ID_TOKEN_COOKIE_SECURE = True
 OIDC_VALID_ISSUERS = [
-    'http://localhost:8080/auth/realms/my_realm'
+    f'{WWW_PROTOCOL}://{SERVER_ADDRESS}:{KEYCLOAK_SERVER_PORT}/auth/realms/{KEYCLOAK_REALM_NAME}'
 ]
-OIDC_OPENID_REALM = 'http://localhost:5000/oidc_callback'
+OIDC_OPENID_REALM = f'{WWW_PROTOCOL}://web:5000/oidc_callback'
+
+with open(OIDC_CLIENT_SECRETS, 'w') as f:
+    json.dump(client_secrets, f, indent=2)
+
 
 # SQL ALCHEMY
 mysql_uri_template = 'mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}'
