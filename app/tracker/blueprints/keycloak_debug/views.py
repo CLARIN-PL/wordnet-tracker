c3404db580e708891891f877c020f599141e0ecf
Jędrzej Jamnicki <jedrzej.jamnicki@gmail.com>
2021-08-17 09:34:26 +0200

    Add realm user creation endpoint for debug purposes

--- app/tracker/blueprints/keycloak_debug/views.py
+++ app/tracker/blueprints/keycloak_debug/views.py
@@ -1,7 +1,14 @@
+import json
+import requests
+import random
 from flask import Blueprint, current_app, url_for
+from httplib2 import Http
+from six.moves.urllib.parse import urlencode
 
 from tracker.extensions import openid_connect
 from flask_oidc.discovery import discover_OP_information
+from config.settings import client_secrets
+from config.kc_admin_endpoints import VIEW_USERS_URI
 
 
 keycloak_debug = Blueprint(
@@ -19,6 +26,63 @@ def home():
         <a href={url_for('keycloak_debug.private')}>/private</a>
         <hr>
     '''
+
+
+@keycloak_debug.route('/users/create/random/<int:num>')
+def create_users(num):
+    ADMIN_CLI_SECRET = '09090d03-28d3-482f-8008-dad35e73b2a1'
+
+    body_dict = {
+        'grant_type': 'client_credentials',
+        'client_id': 'admin-cli',
+        'client_secret': ADMIN_CLI_SECRET
+    }
+
+    _, content = Http().request(
+        uri=client_secrets['web']['token_uri'],
+        method='POST',
+        headers={'Content-Type': 'application/x-www-form-urlencoded'},
+        body=urlencode(body_dict)
+    )
+
+    service_token = json.loads(content.decode('utf-8'))['access_token']
+
+    url = VIEW_USERS_URI.format(
+        hostname=current_app.config['KEYCLOAK_ADDRESS'],
+        port=current_app.config['KEYCLOAK_SERVER_PORT'],
+        realm_name=current_app.config['KEYCLOAK_REALM_NAME'],
+    )
+
+    failed = 0
+    for _ in range(num):
+        random_num = random.randint(1000, 99999)
+        data = {
+            "firstName": f"u{random_num}_firstname",
+            "lastName": f"u{random_num}_lastname",
+            "email": f"u{random_num}@domain.com",
+            "enabled": "true",
+            "username": f"u{random_num}"
+        }
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {service_token}'
+        }
+        try:
+            response = requests.post(
+                url=url,
+                headers=headers,
+                json=data
+            )
+            if response.status_code != 201:
+                failed += 1
+                current_app.logger.warning('User creation failure!')
+
+        except Exception:
+            failed += 1
+            current_app.logger.warning('Sending create user request failed!')
+
+    return f'Created {num-failed} users!'
 
 
 @keycloak_debug.route('/user/info')
